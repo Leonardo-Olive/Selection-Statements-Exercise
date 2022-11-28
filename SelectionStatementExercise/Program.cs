480132b08c933195bda2ddf9e61262263d1e5f04
leonardo oliveira <leonardo_olivesilva@icloud.com>
2022-11-27 16:15:36 -0800

    SelectionExercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let`s play a guessing game!");
+            Console.WriteLine("Now, let`s choose a limit range of the number:");
+
+            var number = Console.ReadLine();
+            var userInput = int.Parse(number);
+            var r = new Random();
+            var favNumber = r.Next(1, userInput);
+
+            Console.WriteLine("Try to guess the number:");
+            var guess = int.Parse(Console.ReadLine());
+
+           
+            if (guess > favNumber)
+            {
+                Console.WriteLine("Too high!Let`s Try it one more time");
+                guess = int.Parse(Console.ReadLine());
+                if (guess != favNumber)
+                {
+                    Console.WriteLine("Sorry, you lost.");
+                }
+            }
+            else if (guess < favNumber)
+            {
+                Console.WriteLine("Too low! One more change.");
+                guess = int.Parse(Console.ReadLine());
+                if (guess != favNumber)
+                {
+                    Console.WriteLine("Sorry, you lost.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            Console.ReadLine();
         }
+
+    
     }
 }
